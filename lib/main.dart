f46c0eb37b6c9e9ab03c6f052cdc739d04d3734d
User <abdiyevnasibali>
2022-05-10 19:24:53 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:lesson_1/screens/detail_screen.dart';
+import 'package:lesson_1/screens/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const HomeScreen(),
+      routes: {
+        HomeScreen.id: (context) => const HomeScreen(),
+        DetailScreen.id: (context) => const DetailScreen(),
+        // /home_screen: HomeScreen()
+      },
+    );
+  }
+}
---
+++ lib/screens/detail_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class DetailScreen extends StatefulWidget {
+  static const id = "/detail_screen";
+  const DetailScreen({Key? key}) : super(key: key);
+
+  @override
+  _DetailScreenState createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("Detail Screen"),
+      ),
+    );
+  }
+}
