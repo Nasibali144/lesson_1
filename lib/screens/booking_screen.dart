2479ff5bf10767dffb961c15b865790c14c43276
User <abdiyevnasibali>
2022-05-11 17:02:40 +0500

    second commit

---
+++ lib/screens/booking_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class BookingScreen extends StatefulWidget {
+  static const id = "/first_screen";
+  const BookingScreen({Key? key}) : super(key: key);
+
+  @override
+  _BookingScreenState createState() => _BookingScreenState();
+}
+
+class _BookingScreenState extends State<BookingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
