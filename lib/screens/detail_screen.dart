011c62988f9e0dfc6a8850556d8cc5e40f458f2a
User <abdiyevnasibali>
2022-05-11 19:35:06 +0500

    fourth commit

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -2,23 +2,39 @@ import 'package:flutter/material.dart';
 
 class DetailScreen extends StatefulWidget {
   static const id = "/detail_screen";
-  const DetailScreen({Key? key}) : super(key: key);
+  final String? name;
+
+  const DetailScreen({Key? key, this.name}) : super(key: key);
 
   @override
   _DetailScreenState createState() => _DetailScreenState();
 }
 
 class _DetailScreenState extends State<DetailScreen> {
+  // field
+
+  // logic method
+  void goBack() {
+    Navigator.of(context).pop("Home Page ga Detail Screen button ni orqali o'tildi!!!");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Detail Screen"),
+        title: Text(widget.name!),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text("Detail Screen"),
+      body: Center(
+        child: MaterialButton(
+          onPressed: goBack,
+          child: Text("Detail Screen"),
+          textColor: Colors.white,
+          color: Colors.blueAccent,
+        ),
       ),
     );
   }
+
+  // widget method
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,23 +10,51 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  String? result;
+
+  void openDetail() async {
+    /// push
+    // I method a:
+    // Navigator.push(context, MaterialPageRoute(builder: (context) => const DetailScreen()));
+    // I method b:
+    // Navigator.of(context).push(MaterialPageRoute(builder: (context) => const DetailScreen(name: "Stadium1",)));
+
+    // II method a:
+    // Navigator.pushNamed(context, DetailScreen.id);
+    // II method b:
+    // Navigator.of(context).pushNamed(DetailScreen.id);
+
+    /// pushReplacement
+    // I method a:
+    // Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const DetailScreen()));
+    // I method b:
+    // Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => const DetailScreen()));
+
+    // II method a:
+    // Navigator.pushReplacementNamed(context, DetailScreen.id);
+    // II method b:
+    // Navigator.of(context).pushReplacementNamed(DetailScreen.id);
+
+    String? value = await Navigator.of(context).push(MaterialPageRoute(builder: (context) => const DetailScreen(name: "Stadium1",)));
+    setState(() {
+      result = value;
+    });
+    print(result ?? "No Data");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: MaterialButton(
-          onPressed: () {
-            // I method a:
-            // Navigator.push(context, MaterialPageRoute(builder: (context) => const DetailScreen()));
-            // I method b:
-            // Navigator.of(context).push(MaterialPageRoute(builder: (context) => const DetailScreen()));
-
-            // II method a:
-            // Navigator.pushNamed(context, DetailScreen.id);
-            // II method b:
-            Navigator.of(context).pushNamed(DetailScreen.id);
-          },
-          child: Text("Stadion 1"),
+          /// bad:
+          // onPressed: () {
+          //   openDetail();
+          // },
+          /// good:
+          onPressed: openDetail,
+          child: Text("Stadion 1 $result"),
           color: Colors.blueAccent,
           textColor: Colors.white,
         ),
